4bb56a208554be315a2ab621b0ff8c9b78e35b5a
sakshimohite02 <sakshimohite44@gmail.com>
2024-10-02 05:02:38 -0700

    Add files via upload

---
+++ Collectionframework/PutMapExample.java
@@ -0,0 +1,14 @@
+package Collectionframework;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PutMapExample {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Apple", 1);
+        map.put("Banana", 2);
+
+        System.out.println("Map after putting entries: " + map);
+    }
+}
